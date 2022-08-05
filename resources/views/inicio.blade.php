64bf11912d69c46061e506bdbbeb396ebc29211f
Daar026m <darwinsantiago2003@hotmail.com>
2022-08-05 14:09:54 -0500

    Inicio JHG

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,21 @@
+@extends('plantilla')
+
+@section('titulo','Inicio')
+
+
+@section('header')
+@endsection
+
+
+@section('contenido')
+    <h2>Prueba de plantilla</h2>
+@endsection
+
+
+@section('scripts')
+    
+@endsection
+
+@section('footer')
+    
+@endsection
---
+++ resources/views/plantilla.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Import google fonts -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+    <title>@yield('titulo')</title>
+
+    @yield('header')
+</head>
+<body>
+
+
+
+@yield('contenido')
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+@yield('scripts')
+
+@yield('footer')
+<footer>PRUEBA PRUEBA PRUEBA</footer>
+
+</body>
+</html>
